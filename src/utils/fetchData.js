d78c3f0aa7a22315d92a1954f3bbe4da53381a9e
atif shaikh <shaikhatif456a@gmail.com>
2023-06-25 17:24:03 +0530

    refactor- changes some code

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions= [
     {
         method: 'GET',
-        url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+       
         headers: {
           'X-RapidAPI-Key': '59fd62b213msh90eef0a4f126a1ep1270bfjsn76a260b8fb47',
           'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
